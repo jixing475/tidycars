de2951f69cd6ed723d004b5b6fd89ae41f6ffaf1
jixing <jixing475@163.com>
2020-03-11 10:02:35 +0800

    Initial commit

---
+++ data-raw/documentation.R
@@ -0,0 +1,32 @@
+#' TidyCars
+#' A data package for TidyCars.
+#' @docType package
+#' @aliases TidyCars-package
+#' @title Package Title
+#' @name TidyCars
+#' @description A description of the data package
+#' @details Use \code{data(package='TidyCars')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{tidy_cars}
+NULL
+
+
+
+#' the stopping distances of cars at different speeds.
+#' @name tidy_cars
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{data.frame} containing the following fields:
+#' \describe{
+#' \item{speed_mph}{}
+#' \item{stopping_distace}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{TidyCars}
+NULL
+
+
+
